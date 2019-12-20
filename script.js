3183f02871a8b3d5e2a00469cd906147849360ac
Jess <jessicatanhc@gmail.com>
2019-12-20 11:36:50 +0800

    Add click letter event

--- script.js
+++ script.js
@@ -3,11 +3,22 @@ var word = "test";
 var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var wordArray = word.split("");
 var letterBoxes = document.querySelectorAll(".letterBox");
+var currentLetter = 0;
+
+//win condition
+var checkLetter = function(){
+      var correctLetter = false;
+      if(this.innerHTML === wordArray[currentLetter]){
+        currentLetter++;
+        correctLetter = true;
+      }
+      return correctLetter;
+}
 
 //add letters and click events to letter box
 var addLetterBoxesProperties = function(){
      for(var i = 0; i< letterBoxes.length ; i ++){
         letterBoxes[i].innerHTML = letters[i];
-        // letterBoxes[i].addEventListener('click',checkLetter);
+         letterBoxes[i].addEventListener('click',checkLetter);
      }
 }
