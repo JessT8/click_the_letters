074d17d2a7a933c2caa2dbe2c7d9e283f4c836a5
Jess <jessicatanhc@gmail.com>
2019-12-20 16:28:00 +0800

    add function to display text

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 //Global Variables
-var word = "something";
-var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var word = "";
+var displayLettersLeft = document.querySelector("#lettersLeft");
+var displayText = function(){
+    word = "SOMETHING";
+    displayLettersLeft.innerText = word;
+}
+displayText();
+var letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var wordArray = word.split("");
 var letterBoxes = document.querySelectorAll(".letterBox");
 var word = document.querySelectorAll(".letterBox");
+
 var pos = [];
+
 
 
 //win condition
 var checkLetter = function(){
       var correctLetter = false;
       var displayClicked = document.querySelector("#lettersClicked");
-      var displayLettersLeft = document.querySelector("#lettersLeft");
       //if current letter click is the letter in the array
       if(this.innerHTML === wordArray[0]){
         correctLetter = true;
         displayClicked.innerText += wordArray.shift();
         displayLettersLeft.innerText = wordArray.join("");
+        // this.style.backgroundcolor = "Grey";
       }
       var displayEndGame = endGame();
       if(displayEndGame){
@@ -46,10 +54,9 @@ var createLetterBox = function(){
     row.appendChild(box);
     letterBoxes = document.querySelectorAll(".letterBox");
 }
-
 var moving = function() {
    for(var i = 0; i< letterBoxes.length ; i ++){
-        if (pos[i] === 900) {
+        if (pos[i] === 887) {
         var randomLetter = Math.floor(Math.random()*letters.length);
         letterBoxes[i].innerHTML = letters[randomLetter];
           pos[i]=0;
