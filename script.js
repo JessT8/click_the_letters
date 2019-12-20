64cf38af289f695087dd107eb7f5954114c0db41
Jess <jessicatanhc@gmail.com>
2019-12-20 11:52:08 +0800

    Add span change when letter click

--- script.js
+++ script.js
@@ -3,14 +3,20 @@ var word = "test";
 var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var wordArray = word.split("");
 var letterBoxes = document.querySelectorAll(".letterBox");
+var word = document.querySelectorAll(".letterBox");
 var currentLetter = 0;
 
 //win condition
 var checkLetter = function(){
       var correctLetter = false;
+      var displayClicked = document.querySelector("#lettersClicked");
+      var displayLettersLeft = document.querySelector("#lettersLeft");
+      //if current letter click is the letter in the array
       if(this.innerHTML === wordArray[currentLetter]){
         currentLetter++;
         correctLetter = true;
+        displayClicked.innerText += wordArray.shift();
+        displayLettersLeft.innerText = wordArray;
       }
       return correctLetter;
 }
