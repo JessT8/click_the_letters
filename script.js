b58437600303f58a9c6829c7a83f0a836946fa84
Jess <jessicatanhc@gmail.com>
2019-12-20 11:22:49 +0800

    Add global variables

---
+++ script.js
@@ -0,0 +1,5 @@
+//Global Variables
+var word = "test";
+var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var wordArray = word.split("");
+var letterBoxes = document.querySelector(".letterBox");
