7b9409585d5b55a821be0654448581b00cb1e4af
Jess <jessicatanhc@gmail.com>
2019-12-20 11:31:12 +0800

    Add letters to letter box

--- script.js
+++ script.js
@@ -2,4 +2,12 @@
 var word = "test";
 var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var wordArray = word.split("");
-var letterBoxes = document.querySelector(".letterBox");
+var letterBoxes = document.querySelectorAll(".letterBox");
+
+//add letters and click events to letter box
+var addLetterBoxesProperties = function(){
+     for(var i = 0; i< letterBoxes.length ; i ++){
+        letterBoxes[i].innerHTML = letters[i];
+        // letterBoxes[i].addEventListener('click',checkLetter);
+     }
+}
